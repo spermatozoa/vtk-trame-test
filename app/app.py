5446a36a2402cbc058f0ef4675968ceb7d6a3334
bhen qiu <showay5987@gmail.com>
2023-04-30 19:57:27 +0800

    it works...?

---
+++ app/app.py
@@ -0,0 +1,5 @@
+from controller.WebServer import WebServer
+
+if __name__ == "__main__":
+    app = WebServer()
+    app.start()
---
+++ app/controller/WebServer.py
@@ -0,0 +1,61 @@
+import os
+import argparse
+
+class WebServer():
+
+    
+    def __init__(self):
+        """ Initialize class
+        
+        Returns: 
+            None
+        """
+        self.base_dir = self.parseArgument().dir
+        self.sub_dir_list, self.sub_files_dict = self.getSubDirFile(self.base_dir)
+
+    def parseArgument(self):
+        """Parse command line argument
+
+        Returns:
+            dict: arguments
+        """
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-b", "--base-dir", help="Directory to vtk files (abs path)", dest="dir", required=True)
+        return parser.parse_args()
+    
+    def getSubDirFile(root_dir):
+        """_summary_
+
+        Args:
+            root_dir (str): abs path of base dir
+
+        Raises:
+            Exception: description of error
+
+        Returns:
+            list: sub dir name in list
+            dict: key = sub dir name, value = list of files name in sub dir
+        """
+        if not root_dir:
+            raise Exception("Base dir should not be empty")
+        sub_dir_files = {}
+        sub_dir_list = []
+        for item in os.listdir(root_dir):
+            sub_path = os.path.join(root_dir, item)
+            if os.path.isdir(sub_path):
+                sub_dir_list.append(item)
+                file_list = []
+                for f in os.listdir(sub_path):
+                    f_path = os.path.join(sub_path, f)
+                    if os.path.isfile(f_path):
+                        file_list.append(f)
+                    elif os.path.isdir(f_path):
+                        print("there is dir in ", sub_path)
+                sub_dir_files[item] = file_list
+            elif os.path.isfile(sub_path):
+                print("there is file in ", root_dir)        
+        if not sub_dir_list:
+            raise Exception("no sub dir")
+        elif not sub_dir_files:
+            raise Exception("no file in sub dir")
+        return sub_dir_list, sub_dir_files
